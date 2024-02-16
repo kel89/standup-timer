dae6893c321ff276909019110132fc77bb1f3dac
Tom Hall <thall1961@gmail.com>
2024-02-16 12:02:14 -0600

    formatting2

--- src/components/timer.js
+++ src/components/timer.js
@@ -65,7 +65,7 @@ export default function Timer({
             </div>
           </div>
           <button
-            className="bg-teal-500 block mx-auto w-200 font-black text-white text-sm mt-5 py-2 px-6 rounded shadow-xl hover:bg-teal-900 hover:shadow-xl transition uppercase"
+            className="bg-teal-500 block mx-auto w-200 font-black text-white text-sm mt-5 tracking-wider py-2 px-12 rounded shadow-xl hover:bg-teal-900 hover:shadow-xl transition uppercase"
             onClick={() => setGoing(!going)}
           >
             toggle
