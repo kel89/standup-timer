b91b0371d55c28c190b644b898bb218e98b10cc2
Tom Hall <thall1961@gmail.com>
2024-03-01 10:46:45 -0600

    fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -14,7 +14,7 @@ export const people = [
   { done: false, name: 'Mike', role: '', imageUrl: null },
   { done: false, name: 'Jeremy', role: '', imageUrl: null },
   { done: false, name: 'Parker', role: '', imageUrl: null },
-  { done: flase, name: 'Sean', role: '', imageUrl: null },
+  { done: false, name: 'Sean', role: '', imageUrl: null },
   { done: false, name: 'Kyle', role: '', imageUrl: null },
   { done: false, name: 'Tom', role: '', imageUrl: null },
   {
